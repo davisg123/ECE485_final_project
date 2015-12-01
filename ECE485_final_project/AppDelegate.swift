0643d822084b200880020c57db55856266331a24
Davis Gossage <dcg13@duke.edu>
2015-11-30 19:46:36 -0500

    Initial Commit

---
+++ ECE485_final_project/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ECE485_final_project
+//
+//  Created by Davis Gossage on 11/30/15.
+//  Copyright © 2015 Davis Gossage. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ECE485_final_project/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ECE485_final_project
+//
+//  Created by Davis Gossage on 11/30/15.
+//  Copyright © 2015 Davis Gossage. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
